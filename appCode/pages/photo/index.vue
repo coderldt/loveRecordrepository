61845ba481cdea2e71fbf60a58c79c57f971a762
lvchao <chao.lv@brgroup.com>
2021-11-05 18:09:22 +0800

    feat: 相册

--- appCode/pages/photo/index.vue
+++ appCode/pages/photo/index.vue
@@ -7,22 +7,32 @@
 				<div>筛选</div>
 			</div>
 		</div>
-		<div class="content" ></div>
+		<div class="content" >
+			<div v-for="(item,index) in list" :key="index">
+				<div>{{item.createTime}}</div>
+				<div class="imgs" v-for="(i,idx) in item.list" :key="idx">
+					<u-image v-for="(img, id) in i.pics" :src="BASE_URL + img" width="250rpx" height="250rpx" mode="aspectFill" border-radius="10"></u-image>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	import { getPhotoList } from '../../apis/photo.js'
+	import { BASE_URL } from '../../config/index.js'
 	export default {
 		data() {
 			return {
-				
+				BASE_URL,
+				list: []
 			}
 		},
 		methods: {
 			async getList() {
 				const res = await getPhotoList()
 				if (res) {
+					this.list = res.list
 					console.log(res);
 				}
 			}
@@ -47,12 +57,18 @@
 				display: flex;
 				align-items: center;
 				justify-content: center;
-				font-size: 30rpx;
+				font-size: 28rpx;
 				color: #b1b1b1;
 				
 				.total {
 					flex: 1;
 				}
+			}
+		}
+		.content {
+			.imgs {
+				display: flex;
+				justify-content: flex-start;
 			}
 		}
 	}
